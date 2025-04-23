c4da9dc3862c2b3ee7255b639b787f4c99f37b81
Mpolani <lassomiguel635@gmail.com>
2025-04-23 10:21:48 -0500

    se crea page de elegir sede

--- customer/src/app/app.routes.ts
+++ customer/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './ui/pages/home/home.component';
+import { LocationComponent } from './ui/pages/location/location.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }, // Página principal
+    { path: '', component: LocationComponent },
+    { path: 'home', component: HomeComponent }, 
     { path: '**', redirectTo: '' }, // Redirección para rutas inválidas
 ];
