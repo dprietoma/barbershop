0a5c245e54e1a19fa4c602f228b626fef310b2f1
fpolania <lassofabian36@gmail.com>
2025-04-30 14:02:02 -0500

    se crea componente steppers

---
+++ customer/src/app/ui/components/steppers/steppers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-steppers',
+  imports: [],
+  templateUrl: './steppers.component.html',
+  styleUrl: './steppers.component.css'
+})
+export class SteppersComponent {
+
+}
