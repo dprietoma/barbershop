85f74acf3eb5516cd3875290c6d19373f8b179f4
fpolania <lassofabian36@gmail.com>
2025-04-30 20:30:25 -0500

    Se crea componente stepper

--- customer/src/app/ui/components/steppers/steppers.component.ts
+++ customer/src/app/ui/components/steppers/steppers.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-steppers',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './steppers.component.html',
   styleUrl: './steppers.component.css'
 })
 export class SteppersComponent {
-
+  @Input() currentStep: number = 0;
+  steps = [
+    { icon: 'bi-scissors', title: 'Selección del servicio' },
+    { icon: 'bi-person-badge', title: 'Selección del barbero' },
+    { icon: 'bi-calendar-event ', title: 'Selección de fecha y hora' },
+    { icon: 'bi-check2-circle', title: 'Confirmación de la cita' },
+    { icon: 'pe-7s-home', title: 'Cita agendada con éxito' }
+  ];
 }
