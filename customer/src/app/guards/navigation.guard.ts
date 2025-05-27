7c3bd6c15db6d501a58bf96e54dfd9493c07e7a9
fpolania <lassofabian36@gmail.com>
2025-05-26 19:04:02 -0500

    se comenta guard

--- customer/src/app/guards/navigation.guard.ts
+++ customer/src/app/guards/navigation.guard.ts
@@ -1,19 +1,19 @@
-import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
-import { NavigationAccessService } from '../ui/utils/global/route-history.service';
+// import { inject } from '@angular/core';
+// import { CanActivateFn, Router } from '@angular/router';
+// import { NavigationAccessService } from '../ui/utils/global/route-history.service';
 
-export const navigationGuard: CanActivateFn = () => {
-  const navAccess = inject(NavigationAccessService);
-  const router = inject(Router);
+// export const navigationGuard: CanActivateFn = () => {
+//   const navAccess = inject(NavigationAccessService);
+//   const router = inject(Router);
 
-  if (navAccess.isAccessAllowed()) {
-    navAccess.reset();
-    return true;
-  }
+//   if (navAccess.isAccessAllowed()) {
+//     navAccess.reset();
+//     return true;
+//   }
 
-  setTimeout(() => {
-    router.navigateByUrl('/home');
-  }, 0);
+//   setTimeout(() => {
+//     router.navigateByUrl('/home');
+//   }, 0);
 
-  return false;
-};
+//   return false;
+// };
