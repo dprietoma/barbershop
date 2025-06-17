14b1ebb54b8fbd26bbadb6317cdd4c1a58e9a8c2
dprietoma <davidfelipeprieto@gmail.com>
2025-06-17 17:05:08 -0500

    se elimina carpeta de admind

--- administrator/.angular/cache/19.2.8/administrator/vite/deps_ssr/dist-VQK2PIBD.js
+++
@@ -1,5 +0,0 @@
-import { createRequire } from 'module';const require = createRequire(import.meta.url);
-import "./chunk-F3ZVRWKR.js";
-import "./chunk-L7HBV277.js";
-import "./chunk-U4L2Y7L5.js";
-import "./chunk-ANGF2IQY.js";
