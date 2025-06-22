f73a0d998f49b39e8ff42d5b45131f58f42ffe78
fpolania <lassofabian36@gmail.com>
2025-06-22 10:59:16 -0500

    merge by footer

--- barbershop-platform/src/app/shared/footer/footer.component.ts
+++ barbershop-platform/src/app/shared/footer/footer.component.ts
@@ -13,23 +13,6 @@ export class FooterComponent implements OnInit {
   data: any
   urlFooter: string;
 
-<<<<<<< HEAD
-  constructor(private appSignal: AppSignalService, private sessionStorage: SessionStorageService,) {
-    effect(() => {
-      this.data = this.appSignal.data();
-      if (this.data?.valor === 'dark') {
-        this.urlFooter = 'https://firebasestorage.googleapis.com/v0/b/barbershop-1e2aa.firebasestorage.app/o/poweredByWhite.svg?alt=media&token=d6e977a1-3f49-48b6-a21c-879d5a3dd7ae'
-      } else if (this.data?.valor === 'light') {
-        this.urlFooter = 'https://firebasestorage.googleapis.com/v0/b/barbershop-1e2aa.firebasestorage.app/o/poweredBy.svg?alt=media&token=d021b561-1089-43fb-bb53-c4d2a66dfe0d';
-      }
-    });
-  }
-
-  ngOnInit() {
-    this.urlFooter = 'https://firebasestorage.googleapis.com/v0/b/barbershop-1e2aa.firebasestorage.app/o/poweredByWhite.svg?alt=media&  oken=d6e977a1-3f49-48b6-a21c-879d5a3dd7ae';
-  }
-
-=======
   constructor(private appSignal: AppSignalService) {
     effect(() => {
       const theme = this.appSignal.data()?.valor || 'light';
@@ -41,7 +24,6 @@ export class FooterComponent implements OnInit {
 
   ngOnInit() { }
 
->>>>>>> 0a9b892004c9ab5eb8a3f69adec8fd6a163a9526
 
 
 }
